c672fd1b5234ca3382fc025386b6361f03e83443
shyl <sohaylae615@gmail.com>
2025-03-14 04:06:36 +0200

    Try num 1 for auto slider

--- src/components/slider/Carousel.tsx
+++ src/components/slider/Carousel.tsx
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState } from "react";
 export default function Carousel() {
   //create state for index 
   const [index ,setIndex] = useState(1);
+  const [transitioning ,setTransitioning] = useState(true);
 
   //use ref for the changes that happens in scrolling for typescript error we need to specfify the correct type for the ref
   const timeOutRef = useRef<number | null>(null);
@@ -16,30 +17,54 @@ export default function Carousel() {
     }
   }
   //clone first slide , and last slide
-  //const cloning  = [ autoSlider[autoSlider.length - 1], ...autoSlider ,  autoSlider[0]];
+  const clonedSlides  = [ autoSlider[autoSlider.length - 1], //clone last slide
+  ...autoSlider, //origin slides  
+  autoSlider[0], // clone first slide
+];
 
   //use effect when the index changes it sets up a timeout that calles a function to increment the index to the next slide 
   useEffect(() => {
     resetTimeOut();
 
     timeOutRef.current = setTimeout(()=>{
-      setIndex((curr) => (curr === autoSlider.length-1 ? 0 : curr + 1));
+      setIndex((prev)=> prev + 1);
     } , delay);
 
     /*
-    clean up function it calls resetTimeOut to clear any existing timeout when the component unmounts or before the next effect runs.
-
-    Purpose: This prevents potential memory leaks and ensures that the timeout is properly cleared, avoiding unexpected behavior when the component is re-rendered or unmounted. */
+    clean up function it calls resetTimeOut to clear any existing timeout when the component unmounts or before the next effect runs. 
+    Purpose: This prevents potential memory leaks and ensures that the timeout is properly cleared, avoiding unexpected behavior when the component is re-rendered or unmounted. 
+    */
     return ()=> {
       resetTimeOut();
     }
   },[index]);
+
+  //handle smooth wrapping 
+  useEffect(() => {
+    // if the slider reached to the last image it jumpes back to the first image
+    if(index === clonedSlides.length -1){
+      setTimeout(() =>{
+        setTransitioning(false);
+        setIndex(1);
+      },1000);
+    } else if (index === 0){
+      setTimeout(() =>{
+        setTransitioning(false);
+        setIndex(clonedSlides.length -2); // last image 
+      },1000);
+    } else {
+      setTransitioning(true);
+    }
+  },[index]);
+
   
   
   return (
-    <div className="w-full h-full flex transition-transform duration-1000 ease-linear" style={{transform: `translateX(-${index * 100}%)` }} >
+    <div className="w-full h-full flex " 
+    style={{transition:` ${transitioning ? "transform 1s ease" : ""}`,
+      transform: `translate3d(${-index * 100}% , 0 , 0)` }} >
     {
-      autoSlider.map(({Id:id , ImageUrl:imageUrl})=> (
+      clonedSlides.map(({Id:id , ImageUrl:imageUrl})=> (
         <div key={id} className="w-full h-full flex-shrink-0">
           <img src={imageUrl} className="w-full h-full object-fill"/>
         </div>
