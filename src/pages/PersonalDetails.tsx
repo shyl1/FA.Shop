6fe5f3b40298a6176bc83612e21ba20eba32451e
shyl <sohaylae615@gmail.com>
2025-05-15 16:15:22 +0300

    personal details UI and Show erroes when there is one

--- src/pages/PersonalDetails.tsx
+++ src/pages/PersonalDetails.tsx
@@ -17,18 +17,16 @@ export default function PersonalDetails() {
 
   // to show errors 
   const [showError , setShowError] = useState<Record<string, string[]>>({});
-  
-  
+
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-
 
   // dont forget to convert phone from string to number cuz e.target.value deal with string only 
   function handleSubmit(e : React.FormEvent){
     // to prevent form from rendering
     e.preventDefault();
     // storing 
-    const result = personalDetailsSchema.safeParse({...formData , phone:Number(formData.phone) });
+    const result = personalDetailsSchema.safeParse({...formData});
 
 
     //check if there is any error
@@ -45,12 +43,10 @@ export default function PersonalDetails() {
     //when clicking submit navigate to billing page
     navigate('/checkout/billing');
   } 
-
   
   // to sync changes
   function handleChange(e: React.ChangeEvent<HTMLInputElement>){
     setFormData({...formData , [e.target.name] : e.target.value});
-
   }
 
   return (
@@ -61,13 +57,23 @@ export default function PersonalDetails() {
           {/* first name */}
           <span className="flex flex-col">
             <label htmlFor="first" className="text-sm font-bold">First Name</label>
-            <input type="text" placeholder="first name" name="firstName" value={formData.firstName} onChange={handleChange} id="first" className="input-form" required/>
+            <input type="text" placeholder="first name" name="firstName" value={formData.firstName} onChange={handleChange} id="first" className={`input-form ${showError.firstName ? "border-red-500" : "border-gray-400"}`} required/>
+
+            {/* show first name error below */}
+            {showError.firstName && (
+              <span className="text-red-500 text-xs mt-1">{showError.firstName[0]}</span>
+            )}
           </span>
 
           {/* last name */}
           <span className="flex flex-col">
             <label htmlFor="last"  className="text-sm font-bold">Last Name</label>
-            <input type="text" placeholder="last name" name="lastName" value={formData.lastName} onChange={handleChange} id="last" className="input-form" required/>
+            <input type="text" placeholder="last name" name="lastName" value={formData.lastName} onChange={handleChange} id="last"  className={`input-form ${showError.lastName ? "border-red-500" : "border-gray-400"}`} required/>
+
+            {/* show last name error below */}
+            {showError.lastName && showError.phone.length > 0 && (
+              <span className="text-red-500 text-xs mt-1">{showError.lastName[0]}</span>
+            )}
           </span>
         </div>
 
@@ -75,20 +81,34 @@ export default function PersonalDetails() {
           {/* email */}
           <span className="flex flex-col">
             <label htmlFor="email"  className="text-sm font-bold">E-mail</label>
-            <input type="email" placeholder="e-mail" name="email" value={formData.email} onChange={handleChange} id="email" className="input-form" required/>
+            <input type="email" placeholder="e-mail" name="email" value={formData.email} onChange={handleChange} id="email"  className={`input-form ${showError.email ? "border-red-500" : "border-gray-400"}`} required/>
+
+            {/* show invalid email error below */}
+            {showError.email && (
+              <span className="text-red-500 text-xs mt-1">{showError.email[0]}</span>
+            )}
           </span>
 
           {/* phone */}
           <span  className="flex flex-col">
             <label htmlFor="phone" className="text-sm font-bold">Phone</label>
-            <input type="text" placeholder="phone" name="phone" id="phone" value={formData.phone} onChange={handleChange} className="input-form" required/>
+            <input type="text" placeholder="phone" name="phone" id="phone" value={formData.phone} onChange={handleChange} className={`input-form ${showError.phone ? "border-red-500" : "border-gray-400"}`} required/>
+
+            {/* show length of phone error below */}
+            {showError.phone && (
+              <span className="text-red-500 text-xs mt-1">{showError.phone[0]}</span>
+            )}
           </span>
         </div>
 
         {/* address */}
         <div className="flex flex-col">
           <label htmlFor="address" className="text-sm font-bold">Address</label>
-          <input type="text"  placeholder="region , state , city , street name , house number and apartment" name="address" id="address" className="border-2 border-gray-400 outline-0 p-2 rounded" onChange={handleChange} value={formData.address} required/>
+          <input type="text"  placeholder="region , state , city , street name , house number and apartment" name="address" id="address" className={`border-2  outline-0 p-2 rounded ${showError.address ? "border-red-400" : "border-gray-400"}`} onChange={handleChange} value={formData.address} required/>
+          {/* show address error below */}
+            {showError.address && (
+              <span className="text-red-500 text-xs mt-1">{showError.address[0]}</span>
+            )}
         </div>
 
         {/* next btn */}
--- src/schemas/personalDetailsSchema.ts
+++ src/schemas/personalDetailsSchema.ts
@@ -1,11 +1,11 @@
 import {z} from "zod";
 
 export const personalDetailsSchema = z.object({
-  firstName: z.string().min(1,'Name is required'),
-  lastName: z.string().min(1 , 'Name is required'),
+  firstName: z.string().min(3,'first name is required'),
+  lastName: z.string().min(3 , 'last name is required'),
   email: z.string().email('invalid email'),
-  phone : z.number().min(11 , 'phone must be 11 digits'),
-  address: z.string().min(1 , 'Address is required'),
+  phone : z.string().min(11 , 'phone must be 11 digits'),
+  address: z.string().min(4, 'Address is required'),
 }); 
 
 
