1cba9a242745487c493f4cdfcbf9f38c56f23528
shyl <sohaylae615@gmail.com>
2025-05-21 02:42:36 +0300

    modal for confimation

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,5 +1,7 @@
 import { useAppSelector } from "@store/hooks"
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { AnimatePresence , motion } from "framer-motion";
 
 type LocationState = {
   fee?: number;
@@ -10,6 +12,9 @@ export default function Confirmation() {
   const {items} = useAppSelector(state => state.CartItem);
 
   const {appliedCode} = useAppSelector(state => state.coupon);
+
+  // show modal 
+  const [showModal , setShowModal] = useState(false);
 
   const location = useLocation();
   const state = location.state as LocationState;
@@ -29,7 +34,11 @@ export default function Confirmation() {
   const total = subTotal + salesTax;
   const grandTotal = (total  + fee) - discount;
 
+  const navigate = useNavigate();
 
+  function hanldeContinueShoppingRediretToHome(){
+    navigate("/");
+  }
 
   return (
     <section className="flex container flex-col justify-center items-center">
@@ -86,9 +95,40 @@ export default function Confirmation() {
 
           {/* btn */}
           <div className="flex justify-end mt-4">
-            <button className="p-2 bg-black text-bague rounded cursor-pointer">confirm order</button>
+            <button className="p-2 bg-black text-bague rounded cursor-pointer" onClick={()=> setShowModal(true)}>confirm order</button>
           </div>
       </div>
+      
+      {/* modal */}
+      <AnimatePresence>
+        {
+        showModal && (
+          <motion.div 
+            key="overlay"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
+            className="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex justify-center items-center z-50"
+          >
+            <motion.div 
+            initial={{ opacity: 0 , scale: 0.8 }}
+            animate={{ opacity: 1 , scale: 1 }}
+            exit={{ opacity: 0 , scale: 0.8  }}
+            transition={{ duration: 0.3 }}
+            className="bg-white p-6 rounded shadow-md md:w-[800px] w-full text-center"
+            >
+              <h3 className="text-xl font-semibold mb-4">Order Confirmed</h3>
+              <p className="mb-4">Your order has been placed!</p>
+              <div className="flex justify-center items-center gap-4">
+                <button onClick={hanldeContinueShoppingRediretToHome} className="p-3 rounded-lg cursor-pointer">Home</button>
+                <button onClick={() => setShowModal(false)} className="bg-yellow p-3 rounded-lg cursor-pointer">Close</button>
+              </div>
+            </motion.div>
+          </motion.div>
+        )
+        }
+      </AnimatePresence>
       
     </section>
   )
